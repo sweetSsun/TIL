fb782e6e22a7563116f58d73f825cfba37849f72
Jisun Kim <nanalong@naver.com>
2022-09-11 23:55:02 +0900

    LinkedList Implementation of add

---
+++ DataStructure/src/list/linkedlist/implementation/Main.java
@@ -0,0 +1,18 @@
+package list.linkedlist.implementation;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		LinkedList numbers = new LinkedList();
+		
+		// 추가
+//		numbers.addFirst(30);
+//		numbers.addFirst(20);
+//		numbers.addFirst(10);
+		numbers.addLast(10);
+		numbers.addLast(20);
+		numbers.addLast(30);
+	}
+
+}

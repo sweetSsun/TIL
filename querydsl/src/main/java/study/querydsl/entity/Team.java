8138e8f99590a71c89c0ff15b44e34f3a7d6ce46
Jisun Kim <nanalong@naver.com>
2022-10-27 23:04:51 +0900

    섹션1,2 : 예제 모델 및 Q-Type 활용

---
+++ querydsl/src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,28 @@
+package study.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본생성자 만들어주는 어노테이션
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id @GeneratedValue
+    private  Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
